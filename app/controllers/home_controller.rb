b8f7ffa3fe1538d3f1f8beca1df0f91784b0baa1
bhaveshrordev <salujabhavesh24@gmail.com>
2025-12-09 16:27:10 +0530

    show earlist event first

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
   before_action :require_clerk_session!
 
   def index
-    @pagy, @events = pagy(:offset, Event.all) 
+    order_sql = <<~SQL.squish
+    CASE
+      WHEN starts_at IS NULL OR starts_at < NOW() THEN 1
+    ELSE 0
+    END,
+    starts_at ASC NULLS LAST
+    SQL
+
+    @pagy, @events = pagy(:offset, Event.order(Arel.sql(order_sql)))
   end
 end
